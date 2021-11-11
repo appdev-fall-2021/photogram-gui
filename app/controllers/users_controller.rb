0c52dcb4ae81b6d9ea460decd3d7c61fe7122a49
avelezme <avelezme@chicagobooth.edu>
2021-11-11 03:14:50 +0000

    user adv

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
 
-def index
- matching_users = User.all
- 
- @list_of_users = matching_users.order({ :username => :asc })
+  def index
+    matching_users = User.all
 
-  render({ :template => "user_templates/index.html.erb"})
-end
+    @list_of_users = matching_users.order({ :username => :asc })
+
+    render({ :template => "user_templates/index.html.erb"})
+  end
+
+  def show
+
+    # Parameters: {"path_username"=>"anisa"}
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username})
+
+    @the_user = matching_usernames.at(0)
+     
+    # .at(0) or .first are the same!
+
+    # if @the_user == nil
+    #   redirect_to("/404")
+
+    # else 
+      render({ :template => "user_templates/show.html.erb"})
+
+    # end
+ 
+  end
 
 end
