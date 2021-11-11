6e9ad547dd79797d4d9d5451135d99cbb48dc19e
avelezme <avelezme@chicagobooth.edu>
2021-11-11 04:05:05 +0000

    setting photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  #users
   get("/users", { :controller => "users", :action => "index" })
-
   get("/users/:path_username", { :controller => "users", :action => "show" })
+
+#photos
+  get("/photos", { :controller => "photos", :action => "index"})
 
 end
